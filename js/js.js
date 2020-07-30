4cf23b059e12ff2f25778561d0d79991fd785bee
Taikiji <francesco.narciso96@gmail.com>
2020-07-30 03:19:13 +0200

    2.0 - Game Designer
    I'm a Game Designer

--- js/js.js
+++ js/js.js
@@ -1,13 +1,28 @@
+$(document).ready(function(){
+  $("#logo").fadeTo(1000, 1);
+  $(".everything").fadeTo(1000, 1);
+});
+
+$(document).ready(function(){
+  $(".everything").fadeTo(1000, 1);
+});
+
+$('.carousel').carousel()
+
+$('.carousel').carousel({
+  interval: 2000
+})
+
 $(function() {
-  var documentEl = $(document), fadeElem = $(".navbar");
+  var documentEl = $(document), fadeElem = $(".navname");
 
   documentEl.on('scroll', function() {
     var currScrollPos = documentEl.scrollTop();
 
     fadeElem.each(function() {
     var $this = $(this),
-      elemOffsetTop = $(".logo").offset().top;
-      $this.css("opacity", 0 + (currScrollPos-elemOffsetTop)/100);
+      elemOffsetTop = $(".pagetop").offset().top;
+      $this.css("opacity", 0 + (currScrollPos-elemOffsetTop)/30);
     });
   });
 
@@ -16,48 +31,12 @@ $(function() {
   setTimeout (function(){
     fadeElem.each(function() {
     var $this = $(this),
-      elemOffsetTop = $(".logo").offset().top;
-      $this.css("opacity", 0 + (currScrollPos-elemOffsetTop)/100);
+      elemOffsetTop = $(".pagetop").offset().top;
+      $this.css("opacity", 0 + (currScrollPos-elemOffsetTop)/30);
     });
   }, 1)
 
 });
-
-
-$(document).ready(function(){
-  $("#logo").fadeTo(1000, 1);
-  $(".everything").fadeTo(1000, 1);
-
-  setTimeout (function(){
-    $(".claim") .fadeTo(3000, 1);
-  }, 1000)
-
-  setTimeout (function(){
-    if ($(window).scrollTop() < 2) {
-      $(".scrolls").fadeTo(1000, 1);
-    }
-  }, 4000)
-
-});
-
-$(function() {
-  var documentEl = $(document), fadeElem = $(".scrolls");
-
-  documentEl.on('scroll', function() {
-    var currScrollPos = documentEl.scrollTop();
-
-    fadeElem.each(function() {
-    var $this = $(this),
-      elemOffsetTop = $(".logo").offset().top;
-      if ($this.css("opacity") > 0) {
-        $this.css("opacity", 1 - (currScrollPos-elemOffsetTop)/300);
-      }
-    });
-
-  });
-
-});
-
 
 //$('.everything').scrollspy();
 
