54770041ef24b7ccd61c3a6445fd33607e46be3c
Taikiji <francesco.narciso96@gmail.com>
2020-09-11 11:50:55 +0200

    Designer :c

--- js/js.js
+++ js/js.js
@@ -1,28 +1,13 @@
-$(document).ready(function(){
-  $("#logo").fadeTo(1000, 1);
-  $(".everything").fadeTo(1000, 1);
-});
-
-$(document).ready(function(){
-  $(".everything").fadeTo(1000, 1);
-});
-
-$('.carousel').carousel()
-
-$('.carousel').carousel({
-  interval: 2000
-})
-
 $(function() {
-  var documentEl = $(document), fadeElem = $(".navname");
+  var documentEl = $(document), fadeElem = $(".navbar");
 
   documentEl.on('scroll', function() {
     var currScrollPos = documentEl.scrollTop();
 
     fadeElem.each(function() {
     var $this = $(this),
-      elemOffsetTop = $(".pagetop").offset().top;
-      $this.css("opacity", 0 + (currScrollPos-elemOffsetTop)/30);
+      elemOffsetTop = $(".logo").offset().top;
+      $this.css("opacity", 0 + (currScrollPos-elemOffsetTop)/100);
     });
   });
 
@@ -31,12 +16,48 @@ $(function() {
   setTimeout (function(){
     fadeElem.each(function() {
     var $this = $(this),
-      elemOffsetTop = $(".pagetop").offset().top;
-      $this.css("opacity", 0 + (currScrollPos-elemOffsetTop)/30);
+      elemOffsetTop = $(".logo").offset().top;
+      $this.css("opacity", 0 + (currScrollPos-elemOffsetTop)/100);
     });
   }, 1)
 
 });
+
+
+$(document).ready(function(){
+  $("#logo").fadeTo(1000, 1);
+  $(".everything").fadeTo(1000, 1);
+
+  setTimeout (function(){
+    $(".claim") .fadeTo(3000, 1);
+  }, 1000)
+
+  setTimeout (function(){
+    if ($(window).scrollTop() < 2) {
+      $(".scrolls").fadeTo(1000, 1);
+    }
+  }, 4000)
+
+});
+
+$(function() {
+  var documentEl = $(document), fadeElem = $(".scrolls");
+
+  documentEl.on('scroll', function() {
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function() {
+    var $this = $(this),
+      elemOffsetTop = $(".logo").offset().top;
+      if ($this.css("opacity") > 0) {
+        $this.css("opacity", 1 - (currScrollPos-elemOffsetTop)/300);
+      }
+    });
+
+  });
+
+});
+
 
 //$('.everything').scrollspy();
 
